8c516b9c3d8f7fdadb06a3d984e2612e45d5e224
devourmealive <foxmanlisichka@yandex.ru>
2022-11-14 18:06:18 +0300

    Initial commit

---
+++ src/main/java/ru/sruit/vultusservice/VultusServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.sruit.vultusservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VultusServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VultusServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sruit/vultusservice/VultusServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sruit.vultusservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VultusServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
