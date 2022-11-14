9b6c5d75b26d0b9fb486edf4b2cfa5a5b25a8c84
devourmealive <foxmanlisichka@yandex.ru>
2022-11-14 21:50:46 +0300

    added password encoder

--- src/main/java/ru/sruit/vultusservice/config/security/WebSecurityConfig.java
+++ src/main/java/ru/sruit/vultusservice/config/security/WebSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,6 +23,11 @@ public class WebSecurityConfig {
                 .and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/**").permitAll().and().build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
