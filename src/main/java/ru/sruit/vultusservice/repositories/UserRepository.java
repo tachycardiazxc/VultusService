5fbebd50dc6f5331cccd04442bf10ed7c3566be6
devourmealive <foxmanlisichka@yandex.ru>
2022-11-14 21:51:36 +0300

    created repositories for entities

---
+++ src/main/java/ru/sruit/vultusservice/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package ru.sruit.vultusservice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.sruit.vultusservice.models.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
