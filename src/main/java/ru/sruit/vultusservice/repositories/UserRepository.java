2ee3ff6ece3b76831f205790cd033fd9e68ca011
devourmealive <foxmanlisichka@yandex.ru>
2022-11-15 19:38:14 +0300

    wrote query fetching suer roles

--- src/main/java/ru/sruit/vultusservice/repositories/UserRepository.java
+++ src/main/java/ru/sruit/vultusservice/repositories/UserRepository.java
@@ -1,12 +1,14 @@
 package ru.sruit.vultusservice.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.sruit.vultusservice.models.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.username = :username")
     User getUserByUsername(String username);
 
 }
