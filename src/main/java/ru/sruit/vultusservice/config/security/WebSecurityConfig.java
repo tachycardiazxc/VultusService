a8b81a3adfa427583f43a2285bbad7252ead4207
devourmealive <foxmanlisichka@yandex.ru>
2022-11-15 19:39:22 +0300

    changed filter chain, added password encoder

--- src/main/java/ru/sruit/vultusservice/config/security/WebSecurityConfig.java
+++ src/main/java/ru/sruit/vultusservice/config/security/WebSecurityConfig.java
@@ -1,42 +1,54 @@
 package ru.sruit.vultusservice.config.security;
 
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.sruit.vultusservice.config.jwt.JwtFilter;
 
-import java.security.Key;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true,
         securedEnabled = true,
         jsr250Enabled = true)
+@RequiredArgsConstructor
 public class WebSecurityConfig {
 
+    private final JwtFilter jwtFiler;
+
     @Bean
-    public SecurityFilterChain getSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .cors()
-                .and().csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/**").permitAll().and().build();
+    GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults(""); // Remove the ROLE_ prefix
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests(
+                        authz -> authz
+                                .antMatchers("/api/auth/login", "/api/auth/token").permitAll()
+                                .anyRequest().authenticated()
+                                .and()
+                                .addFilterAfter(jwtFiler, UsernamePasswordAuthenticationFilter.class)
+                ).build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-    @Bean
-    public Key getJwtSecret() {
-        return Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        return new BCryptPasswordEncoder(15);
     }
 
 }
