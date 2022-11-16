a41d9f37564c9f334f3a46584e4a8317d1d5be2b
devourmealive <foxmanlisichka@yandex.ru>
2022-11-16 04:24:05 +0300

    wrote profile repository query

--- src/main/java/ru/sruit/vultusservice/repositories/ProfileRepository.java
+++ src/main/java/ru/sruit/vultusservice/repositories/ProfileRepository.java
@@ -1,9 +1,14 @@
 package ru.sruit.vultusservice.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.sruit.vultusservice.models.entity.Profile;
 
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
+
+    @Query("SELECT p FROM Profile p JOIN FETCH p.user WHERE p.user.username = :username")
+    Profile getProfileByUserUsername(String username);
+
 }
