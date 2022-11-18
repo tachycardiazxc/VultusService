79ad99734be3e340a27b19cf3bcd98ac51541cc7
devourmealive <foxmanlisichka@yandex.ru>
2022-11-18 21:04:41 +0300

    added swagger docs to auth controller

--- src/main/java/ru/sruit/vultusservice/controllers/AuthRestController.java
+++ src/main/java/ru/sruit/vultusservice/controllers/AuthRestController.java
@@ -1,5 +1,8 @@
 package ru.sruit.vultusservice.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,18 +23,30 @@ public class AuthRestController {
 
     private final AuthService authService;
 
+    @Operation(summary = "Get JWT token by username and password")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Access token and refresh token successfully generated")
+    })
     @PostMapping("/login")
     public Response<JwtResponse> login(@RequestBody LoginJwtRequest authRequest) throws AuthException {
         final JwtResponse token = authService.login(authRequest);
         return Response.ok(token);
     }
 
+    @Operation(summary = "Get new access token by refresh token")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Access token successfully regenerated")
+    })
     @PostMapping("/token")
     public Response<JwtResponse> getNewAccessToken(@RequestBody RefreshJwtRequest request) throws AuthException {
         final JwtResponse token = authService.getAccessToken(request.getRefreshToken());
         return Response.ok(token);
     }
 
+    @Operation(summary = "Get new access and refresh token by refresh token and access token")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Access and refresh token successfully regenerated")
+    })
     @PostMapping("/refresh")
     public Response<JwtResponse> getNewRefreshToken(@RequestBody RefreshJwtRequest request) throws AuthException {
         final JwtResponse token = authService.refresh(request.getRefreshToken());
